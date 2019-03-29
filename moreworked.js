be781b4b8b2fb9369e8b754f5f4bb15f248e27c8
Kurtis Nusbaum <kurtis@uber.com>
2019-03-29 09:40:08 -0700

    initial logic for more worked

---
+++ moreworked.js
@@ -0,0 +1,42 @@
+walk(document.body);
+
+function walk(node)  
+{
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bopen sourced\b/g, "more worked");
+	v = v.replace(/\bopen-sourced\b/g, "more-worked");
+	v = v.replace(/\bopen source\b/g, "more work");
+	v = v.replace(/\bopen-source\b/g, "more-work");
+
+	v = v.replace(/\bOpen Sourced\b/g, "More Worked");
+	v = v.replace(/\bOpen-Sourced\b/g, "More-Worked");
+	v = v.replace(/\bOpen Source\b/g, "More Work");
+	v = v.replace(/\bOpen-Source\b/g, "More-Work");
+	
+	textNode.nodeValue = v;
+}
